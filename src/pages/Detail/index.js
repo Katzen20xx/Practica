105a7c60517ecc0ddbab5682634e87af82a7a819
Katzen20xx <kat.zen.xx@gmail.com>
2020-09-03 17:39:44 -0500

    1 file modified

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Topbar } from './../../components/Topbar'
 import { Title } from './../../components/Title'
+import { CardDetail } from './components/CardDetail'
+import { Button } from '../../components/Button'
 
 export const DetailPage = ()  => (
     <section className='page'>
         <Title label="Parapente San Felix" />
         <CardDetail />
+        <Button />
     </section>
 )
