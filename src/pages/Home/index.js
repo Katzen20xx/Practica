7d7bd30cf2fe7c3636d0eea929058038f4378189
Katzen20xx <kat.zen.xx@gmail.com>
2020-09-03 18:16:23 -0500

    1 file modified, 1 added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Topbar } from './../../components/Topbar'
 import { Title } from './../../components/Title'
 import { Ranking } from './components/Ranking'
+import { FramePage } from '../FramePage'
 
 export const HomePage = () => (
-    <section className="page">
+    <FramePage>
         <Topbar />
         <Title label="Top 5" />
         <Ranking />
         <Title label="Lista" />
-    </section>
+    </FramePage>
 )
