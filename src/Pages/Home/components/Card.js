616d3e503a65f47e890336ebc470d337ba9399c4
Katzen20xx <kat.zen.xx@gmail.com>
2020-09-03 17:07:10 -0500

    2 files modified, 2 added, 1 deleted

--- src/Pages/Home/components/Card.js
+++ src/Pages/Home/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const Card = (props) => (
-    <article style={{ backgroundImage: `url(${props.image})`}}>   // Doble {{}} la primera indica que es codigo js y la segunda para definir objeto
+    <article style={{ backgroundImage: `url(${props.image})`}}>  
         <h2>{ props.description }</h2>
         <h6>{ props.place }</h6>
     </article>
