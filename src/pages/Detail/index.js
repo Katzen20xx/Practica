94dcde9360ab096a7a1d9accbb3f10adaa06c54e
Katzen20xx <kat.zen.xx@gmail.com>
2020-09-03 17:58:20 -0500

    1 file modified

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -3,10 +3,15 @@ import { Title } from './../../components/Title'
 import { CardDetail } from './components/CardDetail'
 import { Button } from '../../components/Button'
 
+const buttonStyle = {
+  backgroundColor: '#FC642D',
+  borderColor: '#fe530c'
+}
+
 export const DetailPage = ()  => (
     <section className='page'>
         <Title label="Parapente San Felix" />
         <CardDetail />
-        <Button />
+        <Button style={ buttonStyle } label='Reserva ahora!' />
     </section>
 )
