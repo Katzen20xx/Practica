2db27ae78cb4a428497f0fe6d3b963c4c3d7d596
Katzen20xx <kat.zen.xx@gmail.com>
2020-09-03 18:49:06 -0500

    3 files modified

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -10,8 +10,8 @@ import { HomePage, BookingPage } from './Booking'
 export const Routes = () => (
     <Switch>
         <Route path="/" exact component={ HomePage } />
-        <Route path="/detail" component={ DetailPage } />
-        <Route path="/booking" component={ BookingPage } />
+        <Route path="/detail/:id" component={ DetailPage } />
+        <Route path="/booking/:id" component={ BookingPage } />
     </Switch>
 )
 
